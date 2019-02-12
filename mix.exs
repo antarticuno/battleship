b777300bff9d2eab726b51b92a1c0a7c9cc38588
Brendan <yang.b@husky.neu.edu>
2019-02-11 21:06:23 -0500

    secrets managed via get_secret

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Battleship.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distiller, "~> 2.0"},
+      {:distillery, "~> 2.0"},
     ]
   end
 end
