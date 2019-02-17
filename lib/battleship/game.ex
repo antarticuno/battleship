0978fc874c5cd5dafee7050fa66717f756f23c1e
maryats <maryats@users.noreply.github.com>
2019-02-17 12:03:12 -0500

    placing works + is tested

--- lib/battleship/game.ex
+++ lib/battleship/game.ex
@@ -8,7 +8,8 @@ defmodule Battleship.Game do
       rankings: [], # player names in order of who lost later -> earlier
       turn: "",      # index of player whose turn it is
       # score: %{},   # { player_name: Nat }
-      boards: %{}   # { player_name : Board }
+      boards: %{},   # { player_name : Board }
+      board_size: %{ width: 10, height: 10 }
     }
   end
 
@@ -20,7 +21,8 @@ defmodule Battleship.Game do
                   |> Enum.map(fn {pn, board} -> {pn, Board.client_board(board)} end)
                   |> Map.new,                          # Map from player_name to Board status
       my_turn: game.turn == player_name,               # boolean for whether turn matches player_name
-      lost: Enum.member?(game.rankings, player_name)   # boolean for whether player_name has been ranked
+      lost: Enum.member?(game.rankings, player_name),   # boolean for whether player_name has been ranked
+      board_size: game.board_size
     }
   end
 
@@ -28,6 +30,10 @@ defmodule Battleship.Game do
 
   defp get_player_board(game, player_name) do
     Map.get(game.boards, player_name)
+  end
+
+  defp set_player_board(game, player_name, new_board) do
+    Map.put(game, :boards, Map.put(game.boards, player_name, new_board))
   end
 
   defp get_player_caterpillars(game, player_name) do
@@ -49,8 +55,9 @@ defmodule Battleship.Game do
   def add_player(game, player_name) do
     game 
     |> Map.put(:players, [player_name | game.players])
+    |> Map.put(:turn, player_name) # TODO only add first player to join?
     # |> Map.put(:score, Map.put(game.score, player_name, 0))
-    |> Map.put(:boards, Map.put(game.boards, player_name, Board.new_board()))
+    |> Map.put(:boards, Map.put(game.boards, player_name, Board.new()))
   end
 
   # Set-Up Phase ----------------------------------------------------------------------------------
@@ -63,56 +70,12 @@ defmodule Battleship.Game do
   def place_caterpillar(game, player_name, type, start_x, start_y, horizontal?) do
     board = get_player_board(game, player_name)
 
-    if (valid_placement?(board, game.board_width, game.board_height, type, start_x, start_y, horizontal?)) do
-      # TODO actually place caterpillar on board
-
+    if (Board.valid_placement?(board, game.board_size.width, game.board_size.height, type, start_x, start_y, horizontal?)) do
+      board = Board.place_caterpillar(board, type, start_x, start_y, horizontal?)
+      game = set_player_board(game, player_name, board)
       {:ok, game}
     else
       {:error, game}  
-    end
-  end
-
-  # ASSUMES: zero-indexed coordinates for start_x and start_y
-  def valid_placement?(board, board_width, board_height, type, start_x, start_y, horizontal?) do
-    length = caterpillar_length(type)
-    end_x = if horizontal?, do: start_x + length, else: start_x
-    end_y = if horizontal?, do: start_y, else: start_y + length
-    
-    in_bounds?(board_width, board_height, start_x, start_y) 
-    && in_bounds?(board_width, board_height, end_x, end_y) 
-    && !intersect?(Map.get(board, :caterpillars), start_x, start_y, end_x, end_y)
-  end
-
-  def in_bounds?(board_width, board_height, x, y) do
-    x >= 0 && x < board_width && y >= 0 && y <= board_height
-  end
-
-  # would the caterpillar defined by start and end intersect with any others on the board?
-  def intersect?(caterpillars, start_x, start_y, end_x, end_y) do
-    caterpillar_coordinates = tween_coordinates(start_x, start_y, end_x, end_y)
-    occupied_coordinates = List.flatten(Map.values(caterpillars))
-    Enum.any?(caterpillar_coordinates, &(Enum.member?(occupied_coordinates, &1)))
-  end
-
-  # create the list of coordinates between start and end
-  def tween_coordinates(start_x, start_y, end_x, end_y) do
-    x_coordinates = Enum.to_list(start_x..end_x)
-    y_coordinates = Enum.to_list(start_y..end_y)
-
-    cond do 
-      (start_y == end_y) -> Enum.map(x_coordinates, fn x -> {x, start_y} end)
-      (start_x == end_x) -> Enum.map(y_coordinates, fn y -> {start_x, y} end)
-      true -> Enum.zip(x_coordinates, y_coordinates) # ASSUME: perfect diagonal
-    end
-  end
-
-  defp caterpillar_length(type) do
-    case type do
-      :carrier -> 5
-      :battleship -> 4
-      :cruiser -> 3
-      :submarine -> 3
-      :destroyer -> 2
     end
   end
 
@@ -122,6 +85,7 @@ defmodule Battleship.Game do
     {x, _} = Integer.parse(x)
     sting(game, opponent, stringify_posn(x, y))
   end
+
   def sting(game, opponent, target) do
     board = game.boards
     |> Map.get(opponent)    #get the Board for opponent
@@ -131,7 +95,7 @@ defmodule Battleship.Game do
     end
 
     game
-    |> Map.put(:boards, Map.put(game.boards, opponent, board))
+    |> set_player_board(opponent, board)
     |> next_player
   end
   
