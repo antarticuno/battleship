ea02129f3f24060f8c0619e51bdda30d37cbf868
Brendan <yang.b@husky.neu.edu>
2019-02-16 23:37:43 -0500

    client_view fixed, score commented out, board.ex client_board

--- lib/battleship/board.ex
+++ lib/battleship/board.ex
@@ -14,18 +14,32 @@ defmodule Battleship.Board do
     }
   end
 
-  def hit?(board, target) do
+  def client_board(board), do: board.status
+
+  def update_board(board, target) do
     status = board.status
-    if (Map.has_key?(status, target)) do 
-      Map.get(status, target) == "hit"
+    if (Map.has_key?(status, target)) do
+      board #TODO do we be nice? or just return the board
     else
-      board.caterpillars
-      |> Map.values
-      |> Enum.reduce(false, &(Enum.member?(&1, target) or &2))
+      if hit?(board, target) do
+        board
+        |> Map.put(:status, Map.put(status, target, "hit"))
+      else
+        board
+        |> Map.put(:status, Map.put(status, target, "miss"))
+      end
     end
   end
 
-  def dead?(caterpillar, status) do
+  # Assumes sting hasn't been made already
+  defp hit?(board, target) do
+    status = board.status
+    board.caterpillars
+    |> Map.values
+    |> Enum.reduce(false, &(Enum.member?(&1, target) or &2))
+  end
+
+  defp dead?(caterpillar, status) do
     caterpillar
     |> Enum.reduce(true, &(Map.get(status, &1) == "hit"))
   end
--- lib/battleship/game.ex
+++ lib/battleship/game.ex
@@ -2,7 +2,6 @@ defmodule Battleship.Game do
  
   alias Battleship.Board
 
-
   def new do
     %{
       players: [],  # player names
@@ -15,13 +14,13 @@ defmodule Battleship.Game do
 
   def client_view(game, player_name) do
     {me, opponents} = Map.split(game.boards, [player_name])
-    stat = Map.get(myBoard, :caterpillers)
-    lost = Enum.reduce(Map.values(stat), true,  fn {v, acc} -> acc and dead?(stat, v) end)
     %{
-      my_board: myBoard, # Map from player_name to Board
-      opponents:  Enum.each(opponentBoards, fn {k, v} -> {k, Map.get(v, :status)} end),
-      my_turn: current_turn?(game, player_name),
-      lost: Enum.member?(game.rankings, player_name)
+      my_board: me,                                    # Map from player_name to Board
+      opponents: opponents
+                 |> Enum.map(fn {pn, board} -> {pn, Board.client_board(board)} end)
+                 |> Map.new,                           # Map from player_name to Board status
+      my_turn: current_turn?(game, player_name),       # boolean for whether turn matches player_name
+      lost: Enum.member?(game.rankings, player_name)   # boolean for whether player_name has been ranked
     }
   end
 
@@ -32,19 +31,19 @@ defmodule Battleship.Game do
   def sting(game, opponent, target) do
     board = game.boards
     |> Map.get(opponent)    #get the Board for opponent
-    |> update_board(target) #delegate to Board to update_board
-    if lost?(board) do 
+    |> Board.update_board(target) #delegate to Board to update_board
+    if Board.lost?(board) do 
       game = Map.put(game, :rankings, [opponent | Map.get(game, :rankings)])
     end
 
-    updated_game = game
+    game
     |> Map.put(:boards, Map.put(game.boards, opponent, board))
-    |> Map.put(:turn, rem(Map.get(game, :turn) + 1), remaining_players(game))
+    |> Map.put(:turn, rem(Map.get(game, :turn) + 1, remaining_players(game)))
   end
 
-  def remaining_players(game), do: length(game.players -- game.ranking)
+  defp remaining_players(game), do: length(game.players -- game.ranking)
   
-  def stringify_posn(x, y), do: <<65+x>> <> y
+  defp stringify_posn(x, y), do: <<65+x>> <> y
 
   def add_player(game, player_name) do
     game 
