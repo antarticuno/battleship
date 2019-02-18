196e82981044f0c59a8222567d21c7d202dc2ebd
maryats <maryats@users.noreply.github.com>
2019-02-18 13:22:29 -0500

    add render helper methods

--- assets/js/battleship.jsx
+++ assets/js/battleship.jsx
@@ -32,66 +32,88 @@ class Battleship extends React.Component {
   }
 
   render() {
+    console.log("state", this.state);
     switch (this.state.phase) {
       case "joining":
-	return (<div className="container">Waiting for other players to join...</div>);
+	      this.renderJoining();
+        break;
       case "setup":
-        return (
-	  <div className="container">
-	    <div className="row">
-	      <div className="col">
-              <form>
-		  Caterpillar: <select name="caterpillar">
-		    <option value="carrier">Carrier</option>
-		    <option value="battleship">Battleship</option>
-		    <option value="cruiser">Cruiser</option>
-		    <option value="submarine">Submarine</option>
-		    <option value="destroyer">Destroyer</option>
-		  </select>
-		  Start X: <input type="text" maxlength={this.state.boardSize.width} />
-		  Start Y: <input type="text" maxlength={this.state.boardSize.height} />
-		  Direction: <select>
-		    <option value="true">Horizontal</option>
-		    <option value="false">Vertical</option>
-		  </select>
-	          <button onClick={this.on_place.bind(this)}>Place!</button>
-	        </form>
-	      </div>
-	      <div className="col">
-		<PlayerBoard myBoard={this.state.myBoard}/>
-	      </div>
-	    </div>
-	  </div>
-	);
+        this.renderSetup();
+        break;
       case "playing":
-        return (
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <EnemyBoards />
-              </div>
-	      <div className="col">
-	        <div className="row">
-	          <div className="col">
-	            <PlayerInput />
-	          </div>
-	          <div className="col">
-	            <PlayerBoard />
-	          </div>
-  	        </div>
-	      </div>
-            </div>
-          </div>
-        );
+        this.renderPlaying();
+        break;
       case "gameover":
-	return (<ScoreBoard rankings={this.state.rankings} />);
+	      this.renderGameOver();
+        break;
       default:
-	return (<div className="container">Waiting for next phase...</div>);
+	      return (<div className="container">Waiting for next phase...</div>);
     }
   }
 
   gotView(view) {
+    console.log(view.game);
     this.setState(view.game);
+  }
+
+  renderJoining() {
+    return (<div className="container">Waiting for other players to join...</div>);
+  }
+
+  renderPlaying() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <EnemyBoards />
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <PlayerInput />
+              </div>
+              <div className="col">
+                <PlayerBoard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderSetup() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form>
+              Caterpillar: <select name="caterpillar">
+                <option value="carrier">Carrier</option>
+                <option value="battleship">Battleship</option>
+                <option value="cruiser">Cruiser</option>
+                <option value="submarine">Submarine</option>
+                <option value="destroyer">Destroyer</option>
+              </select>
+              Start X: <input type="text" maxlength={this.state.boardSize.width} />
+              Start Y: <input type="text" maxlength={this.state.boardSize.height} />
+              Direction: <select>
+              <option value="true">Horizontal</option>
+              <option value="false">Vertical</option>
+            </select>
+            <button onClick={this.on_place.bind(this)}>Place!</button>
+          </form>
+        </div>
+        <div className="col">
+          <PlayerBoard myBoard={this.state.myBoard}/>
+        </div>
+      </div>
+    </div>
+    );
+  }
+
+  renderGameOver() {
+    return (<ScoreBoard rankings={this.state.rankings} />);
   }
 }
 
