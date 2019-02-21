9bea4b53602540dcf67c8a4aacb7be063f86ba9d
maryats <maryats@users.noreply.github.com>
2019-02-21 12:34:23 -0500

    clientside prevents restinging the same place

--- assets/js/boards.jsx
+++ assets/js/boards.jsx
@@ -58,7 +58,7 @@ function Board(props) {
         let isMiss = s == "miss";
       	let isDead = s == "dead";
         let hit = isHit ? " hit" : isMiss ? " miss" : isDead ? "dead" : "";
-        let onClickSting = () => {if (onClick != null) {onClick(r, c, props.name);};};
+        let onClickSting = () => {if (onClick != null && !isHit && !isMiss && !isDead) {onClick(r, c, props.name);};};
 
         col.push(<div key={r} onClick={onClickSting.bind(this)} className={coord + " column board-cell " + (caterpillars.includes(coord) ? "caterpillar " : "") + hit}>
           {isHit ? "X" : isMiss ? "O" : isDead ? "D" : " "}
